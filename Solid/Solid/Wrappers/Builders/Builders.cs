e1d0a13cb85167090479f54ed366e4e5b29928b1
Greg <life.gregr@gmail.com>
2013-04-19 21:50:04 +0300

    Ignore this.

--- Solid/Solid/Wrappers/Builders/Builders.cs
+++ Solid/Solid/Wrappers/Builders/Builders.cs
@@ -4,11 +4,23 @@ namespace Solid.Builders
 {
 	static class Builders
 	{
+		/// <summary>
+		/// Returns an object that implicitly constructs a collection of type FlexibleList from the specified sequence..
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <param name="seq">The sequence.</param>
+		/// <returns></returns>
 		public static FlexListBuilder<T> BuildFlexList<T>(this IEnumerable<T> seq)
 		{
 			return new FlexListBuilder<T>(seq);
 		}
 
+		/// <summary>
+		///  Returns an object that implicitly constructs a collection of type FlexibleList from the specified sequence.
+		/// </summary>
+		/// <typeparam name="T">The type of value.</typeparam>
+		/// <param name="seq">The seq.</param>
+		/// <returns></returns>
 		public static VectorBuilder<T> BuildVector<T>(this IEnumerable<T> seq)
 		{
 			return new VectorBuilder<T>(seq);
